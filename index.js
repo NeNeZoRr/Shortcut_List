1f20e505d23da974cfc1c0cc7670951707c7abca
NeNeZoRr <zorrillalin@gmail.com>
2023-10-21 11:04:54 -0400

    Update 3 	modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express')
 
 const app = express()
+
+app.get('/', function (req, res) {
+    // This gets sent to the client 
+    // (your web browser most likely!)
+    res.send('Hello world')
+})
+
+app.get('/second', function (req, res) {
+    res.send('My Second Page!')
+})
+
+app.listen(3000, function () {
+    console.log('I am awake!')
+})
